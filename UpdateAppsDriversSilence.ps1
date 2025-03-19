90ec405493a92ea502d36b40202cb71e7a6f6f5a
danny9001 <danny9001@gmail.com>
2025-03-19 16:07:20 -0400

    Update UpdateAppsDriversSilence.ps1

--- UpdateAppsDriversSilence.ps1
+++ UpdateAppsDriversSilence.ps1
@@ -1,11 +1,9 @@
 # Actualizacion de Aplicaciones automatica y Drivers desde Windows Update
 # Autor: Daniel Landivar
 # Licencia: CC BY-NC (Reconocimiento-NoComercial)
-
 # Créditos:
 # Editor: Microsoft Copilot
 # RuckZuck: https://github.com/rzander/ruckzuck, https://ruckzuck.tools/
-# Chocolatey: https://chocolatey.org
 # Winget: https://github.com/microsoft/winget-cli
 # Windows Update: Microsoft
 
@@ -15,69 +13,16 @@ function Is-Server {
     return $os.ProductType -ne 1
 }
 
-# Function to update Windows Store apps
-function Update-WindowsStoreApps {
-    if (Is-Server) {
-        Write-Log "Sistema operativo detectado como servidor. Omitiendo actualizaciones de la Tienda Windows."
-        return
-    }
-
-    if (Get-Command Get-CimInstance -ErrorAction SilentlyContinue) {
-        Write-Log "Actualizando aplicaciones de la Tienda Windows..."
-        try {
-            Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" |
-            Invoke-CimMethod -MethodName UpdateScanMethod
-            Write-Log "Actualización de aplicaciones de la Tienda Windows completada."
-        } catch {
-            Write-Log "Error al actualizar aplicaciones de la Tienda Windows: $_"
-        }
-    } else {
-        Write-Log "La Tienda Windows no está disponible. Saltando actualizaciones de la Tienda Windows."
-    }
-}
-
-# Function to update Chocolatey packages
-function Update-ChocolateyApps {
-    if (Get-Command choco -ErrorAction SilentlyContinue) {
-        Write-Log "Actualizando paquetes de Chocolatey..."
-        try {
-            choco upgrade all -y
-            Write-Log "Actualización de paquetes de Chocolatey completada."
-        } catch {
-            Write-Log "Error al actualizar paquetes de Chocolatey: $_"
-        }
-    } else {
-        Write-Log "Chocolatey no está instalado. Instalando Chocolatey..."
-        try {
-            Set-ExecutionPolicy Bypass -Scope Process -Force
-            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-            Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-            Write-Log "Chocolatey instalado correctamente."
-            Update-ChocolateyApps
-        } catch {
-            Write-Log "Error al instalar Chocolatey: $_"
-        }
-    }
-}
-
-# Function to update Winget packages
-function Update-WingetApps {
-    if (Is-Server) {
-        Write-Log "Sistema operativo detectado como servidor. Omitiendo actualizaciones de Winget."
-        return
-    }
-
-    if (Get-Command winget -ErrorAction SilentlyContinue) {
-        Write-Log "Actualizando paquetes de Winget..."
-        try {
-            winget upgrade --all
-            Write-Log "Actualización de paquetes de Winget completada."
-        } catch {
-            Write-Log "Error al actualizar paquetes de Winget: $_"
-        }
-    } else {
-        Write-Log "Winget no está instalado. Saltando actualizaciones de Winget."
-    }
+# Function to log messages
+function Log-Message {
+    param (
+        [string]$message,
+        [string]$color = "White"
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logMessage = "$timestamp - $message"
+    Write-Host $logMessage -ForegroundColor $color
+    Add-Content -Path "$PSScriptRoot\UpdateLog.txt" -Value $logMessage
 }
 
 # Function to update RuckZuck packages
@@ -90,115 +35,159 @@ function Update-RuckZuckApps {
         try {
             $currentVersion = (& $rzPath --version).Split(" ")[-1]
             if ($currentVersion -ne $latestVersion) {
-                Write-Log "RuckZuck no está actualizado. Descargando la última versión..."
+                Log-Message "RuckZuck no está actualizado. Descargando la última versión..." "Yellow"
                 Invoke-WebRequest -Uri $rzUrl -OutFile $rzPath
-                Write-Log "RuckZuck actualizado correctamente."
+                Log-Message "RuckZuck actualizado correctamente." "Green"
             }
         } catch {
-            Write-Log "Error al obtener la versión de RuckZuck: $_"
+            Log-Message "Error al obtener la versión de RuckZuck: $_" "Red"
         }
     } else {
-        Write-Log "RuckZuck no está instalado. Instalando RuckZuck..."
+        Log-Message "RuckZuck no está instalado. Instalando RuckZuck..." "Yellow"
         Invoke-WebRequest -Uri $rzUrl -OutFile $rzPath
-        Write-Log "RuckZuck descargado correctamente."
+        Log-Message "RuckZuck descargado correctamente." "Green"
     }
 
     try {
-        Write-Log "Actualizando paquetes de RuckZuck..."
-        & $rzPath update --all
-        Write-Log "Actualización de paquetes de RuckZuck completada."
+        Log-Message "Actualizando paquetes de RuckZuck..." "Cyan"
+        & $rzPath update --all --retry --user
+        Log-Message "Actualización de paquetes de RuckZuck completada." "Green"
     } catch {
-        Write-Log "Error al actualizar paquetes de RuckZuck: $_"
+        Log-Message "Error al actualizar paquetes de RuckZuck: $_" "Red"
+    }
+
+    try {
+        Log-Message "Instalando paquetes no detectados..." "Cyan"
+        $missingUpdates = & $rzPath update --list --all --user
+        foreach ($update in $missingUpdates) {
+            & $rzPath install --name $update.ProductName --vendor $update.Manufacturer --version $update.ProductVersion
+        }
+        Log-Message "Instalación de paquetes no detectados completada." "Green"
+    } catch {
+        Log-Message "Error al instalar paquetes no detectados: $_" "Red"
+    }
+}
+
+# Function to update Winget packages
+function Update-WingetApps {
+    Log-Message "Verificando la disponibilidad de Winget..." "Cyan"
+
+    if (Get-Command winget -ErrorAction SilentlyContinue) {
+        Log-Message "Actualizando paquetes de Winget..." "Cyan"
+        try {
+            winget upgrade --all --include-unknown --accept-package-agreements --force
+            Log-Message "Actualización de paquetes de Winget completada." "Green"
+        } catch {
+            Log-Message "Error al actualizar paquetes de Winget: $_" "Red"
+        }
+    } else {
+        Log-Message "Winget no está instalado. Instalando Winget..." "Yellow"
+        try {
+            Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+            Add-AppxPackage -Path "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+            Log-Message "Winget instalado correctamente." "Green"
+            Update-WingetApps
+        } catch {
+            Log-Message "Error al instalar Winget: $_" "Red"
+        }
+    }
+}
+
+# Function to update Windows Store apps
+function Update-WindowsStoreApps {
+    if (Is-Server) {
+        Log-Message "Sistema operativo detectado como servidor. Omitiendo actualizaciones de la Tienda Windows." "Yellow"
+        return
+    }
+
+    if (Get-Command Get-CimInstance -ErrorAction SilentlyContinue) {
+        Log-Message "Actualizando aplicaciones de la Tienda Windows..." "Cyan"
+        try {
+            $apps = Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01"
+            if ($apps) {
+                $apps | Invoke-CimMethod -MethodName UpdateScanMethod
+                Log-Message "Actualización de aplicaciones de la Tienda Windows completada." "Green"
+            } else {
+                Log-Message "No se encontraron aplicaciones para actualizar." "Yellow"
+            }
+        } catch {
+            Log-Message "Error al actualizar aplicaciones de la Tienda Windows: $_" "Red"
+        }
+    } else {
+        Log-Message "La Tienda Windows no está disponible. Saltando actualizaciones de la Tienda Windows." "Yellow"
     }
 }
 
 # Function to update drivers from Windows Update
 function Update-Drivers {
-    Write-Log "Se conectó a Windows Update en busca de drivers..."
+    Log-Message "Se conectó a Windows Update en busca de drivers y actualizaciones..." "Cyan"
     $UpdateSearcher = New-Object -Com Microsoft.Update.Searcher
-    Write-Log "Se inició la búsqueda de drivers..."
+    Log-Message "Se inició la búsqueda de drivers y actualizaciones..." "Cyan"
     $SearchResult = $UpdateSearcher.Search("IsInstalled=0")
 
     # Inicializar la barra de progreso
     $progress = 0
     $totalUpdates = $SearchResult.Updates.Count
-
     $UpdatesToDownload = New-Object -Com Microsoft.Update.UpdateColl
+
     foreach ($Update in $SearchResult.Updates) {
         $UpdatesToDownload.Add($Update) | Out-Null
-        Write-Log "Encontrado controlador: $($Update.Title)"
+        Log-Message "Encontrado controlador o Actualizacion: $($Update.Title)" "Cyan"
         $progress++
         Write-Progress -Activity "Buscando drivers" -Status "$progress de $totalUpdates encontrados" -PercentComplete (($progress / $totalUpdates) * 100)
     }
 
     # Verificación de que la variable $UpdatesToDownload no esté vacía
     if ($UpdatesToDownload.Count -eq 0) {
-        Write-Log "No hay actualizaciones disponibles para descargar."
+        Log-Message "No hay actualizaciones disponibles para descargar." "Yellow"
         return
     }
 
     try {
-        Write-Log "Iniciando sesión de actualización..."
+        Log-Message "Iniciando sesión de actualización..." "Cyan"
         $UpdateSession = New-Object -Com Microsoft.Update.Session
         $Downloader = $UpdateSession.CreateUpdateDownloader()
         $Downloader.Updates = $UpdatesToDownload
-
         if (-not $Downloader.Updates) {
             throw "No hay actualizaciones para descargar."
         }
-
-        Write-Log "Descargando actualizaciones..."
+        Log-Message "Descargando actualizaciones..." "Cyan"
         $Downloader.Download()
-        Write-Log "Descarga completada."
+        Log-Message "Descarga completada." "Green"
     } catch {
-        Write-Log "Error al descargar las actualizaciones: $_"
+        Log-Message "Error al descargar las actualizaciones: $_" "Red"
         return
     }
 
     # Verificar si los controladores están descargados y desencadenar la instalación
     try {
         $UpdatesToInstall = New-Object -Com Microsoft.Update.UpdateColl
-
         $UpdatesToDownload | ForEach-Object {
             if ($_.IsDownloaded) {
                 $UpdatesToInstall.Add($_) | Out-Null
-                Write-Log "Instalando controlador: $($_.Title)"
+                Log-Message "Instalando controlador: $($_.Title)" "Cyan"
             }
         }
-
         if ($UpdatesToInstall.Count -eq 0) {
             throw "No hay actualizaciones descargadas para instalar."
         }
-
-        Write-Log 'Instalando controladores...'
+        Log-Message 'Instalando controladores o actualizacion...' "Green"
         $Installer = $UpdateSession.CreateUpdateInstaller()
         $Installer.Updates = $UpdatesToInstall
         $InstallationResult = $Installer.Install()
-
         if ($InstallationResult.RebootRequired) {
-            Write-Log '¡Reinicio requerido! Por favor, reinicie ahora.'
+            Log-Message '¡Reinicio requerido! Por favor, reinicie ahora.' "Red"
         } else {
-            Write-Log 'Instalación completada.'
+            Log-Message 'Instalación completada.' "Green"
         }
     } catch {
-        Write-Log "Error durante la instalación: $_"
+        Log-Message "Error durante la instalación: $_" "Red"
     }
 }
 
-# Function to write logs
-function Write-Log {
-    param (
-        [string]$message
-    )
-    $logFile = "$PSScriptRoot\Logs\UpdateLog_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
-    Add-Content -Path $logFile -Value $message
-}
-
 # Main execution
-Write-Log "Iniciando actualizaciones automáticas de aplicaciones y drivers..."
-Update-WindowsStoreApps
-Update-ChocolateyApps
-Update-WingetApps
+Log-Message "Iniciando actualizaciones automáticas de aplicaciones y drivers..." "Cyan"
 Update-RuckZuckApps
+Update-WingetApps
+Update-WindowsStoreApps
 Update-Drivers
-Write-Log "Proceso de actualización completado."
